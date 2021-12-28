057c903e7c73ab4da578f1577f96736aa3640185
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2021-12-28 18:49:57 +0000

    Add container and styling

--- src/components/App/Authenticated.tsx
+++ src/components/App/Authenticated.tsx
@@ -1,35 +1,43 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Dashboard from "../Dashboard/DashBoard";
-import { UserInterface } from "../auth/useUser";
-import AuthenticatedNavBar from "../NavBar/AuthenticatedNavBar";
-import Container from "@material-ui/core/Container";
-import EditTask from "../EditTask/EditTask";
-import CreateTask from "../CreateTask/CreateTask";
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Dashboard from '../Dashboard/DashBoard'
+import { UserInterface } from '../auth/useUser'
+import AuthenticatedNavBar from '../NavBar/AuthenticatedNavBar'
+import Container from '@material-ui/core/Container'
+import EditTask from '../EditTask/EditTask'
+import CreateTask from '../CreateTask/CreateTask'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    mainContainer: {
+      height: 'calc(100vh - 64px)',
+      overflowY: 'auto'
+    }
+  })
+)
 
 const Authenticated = (props: { setUser: (user: UserInterface) => void }) => {
-	return (
-		<>
-			<Router>
-				<AuthenticatedNavBar setUser={props.setUser} />
-				<Container maxWidth="md">
-					<Route
-						exact
-						path="/"
-						render={(routeProps: any) => (
-							<Dashboard
-								{...routeProps}
-								setUser={props.setUser}
-							/>
-						)}
-					/>
-					<Route path="/tasks/edit/:id" component={EditTask} />
-					<Route path="/tasks/new" component={CreateTask} />
-				</Container>
-			</Router>
-		</>
-	);
-};
+  const classes = useStyles()
 
-export default Authenticated;
+  return (
+    <>
+      <Router>
+        <AuthenticatedNavBar setUser={props.setUser} />
+        <Container className={classes.mainContainer} maxWidth='md'>
+          <Route
+            exact
+            path='/'
+            render={(routeProps: any) => (
+              <Dashboard {...routeProps} setUser={props.setUser} />
+            )}
+          />
+          <Route path='/tasks/edit/:id' component={EditTask} />
+          <Route path='/tasks/new' component={CreateTask} />
+        </Container>
+      </Router>
+    </>
+  )
+}
+
+export default Authenticated
--- src/components/App/Unauthenticated.tsx
+++ src/components/App/Unauthenticated.tsx
@@ -1,33 +1,51 @@
-import React from "react";
-import Login from "../Login/Login";
-import SignUp from "../SignUp/SignUp";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { UserInterface } from "../auth/useUser";
-import UnauthenticatedNavBar from "../NavBar/UnauthenticatedNavBar";
-import Landing from "../Landing/Landing";
+import React from 'react'
+import Login from '../Login/Login'
+import SignUp from '../SignUp/SignUp'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { UserInterface } from '../auth/useUser'
+import UnauthenticatedNavBar from '../NavBar/UnauthenticatedNavBar'
+import Landing from '../Landing/Landing'
+import Container from '@material-ui/core/Container'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    mainContainer: {
+      height: 'calc(100vh - 64px)',
+      overflowY: 'auto',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  })
+)
 
 const Unauthenticated = (props: { setUser: (user: UserInterface) => void }) => {
-	return (
-		<>
-			<Router>
-				<UnauthenticatedNavBar />
-				<Route exact path="/" component={Landing} />
-				<Route
-					exact
-					path="/login"
-					render={(routeProps) => (
-						<Login {...routeProps} setUser={props.setUser} />
-					)}
-				/>
-				<Route
-					path="/signup"
-					render={(routeProps) => (
-						<SignUp {...routeProps} setUser={props.setUser} />
-					)}
-				/>
-			</Router>
-		</>
-	);
-};
+  const classes = useStyles()
 
-export default Unauthenticated;
+  return (
+    <>
+      <Router>
+        <UnauthenticatedNavBar />
+        <Container className={classes.mainContainer} maxWidth='md'>
+          <Route exact path='/' component={Landing} />
+          <Route
+            exact
+            path='/login'
+            render={(routeProps) => (
+              <Login {...routeProps} setUser={props.setUser} />
+            )}
+          />
+          <Route
+            path='/signup'
+            render={(routeProps) => (
+              <SignUp {...routeProps} setUser={props.setUser} />
+            )}
+          />
+        </Container>
+      </Router>
+    </>
+  )
+}
+
+export default Unauthenticated
