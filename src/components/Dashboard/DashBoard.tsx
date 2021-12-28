8b09729960e7a2bc14c6c773b38960aed0fd3530
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2021-12-28 18:22:24 +0000

    remove unused grid item

--- src/components/Dashboard/DashBoard.tsx
+++ src/components/Dashboard/DashBoard.tsx
@@ -1,22 +1,21 @@
-import React from "react";
-import useUser from "../auth/useUser";
-import UserTasks from "../UserTasks/UserTasks";
-import Grid from "@material-ui/core/Grid";
+import React from 'react'
+import useUser from '../auth/useUser'
+import UserTasks from '../UserTasks/UserTasks'
+import Grid from '@material-ui/core/Grid'
 
 const DashBoard = () => {
-	const { user } = useUser();
+  const { user } = useUser()
 
-	return (
-		<Grid container spacing={3}>
-			<Grid item xs={12}>
-				<h1>Welcome {user.user!.name}</h1>
-			</Grid>
-			<Grid item xs={12}>
-				<UserTasks />
-			</Grid>
-			<Grid item xs={12}></Grid>
-		</Grid>
-	);
-};
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <h1>Welcome {user.user!.name}</h1>
+      </Grid>
+      <Grid item xs={12}>
+        <UserTasks />
+      </Grid>
+    </Grid>
+  )
+}
 
-export default DashBoard;
+export default DashBoard
