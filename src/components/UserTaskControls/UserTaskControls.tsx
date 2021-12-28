7162a8a2d31c18127c2f16a83abe13c03fbabded
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2021-12-28 18:27:04 +0000

    code formatting

--- src/components/UserTaskControls/UserTaskControls.tsx
+++ src/components/UserTaskControls/UserTaskControls.tsx
@@ -1,117 +1,113 @@
-import React, { SetStateAction, Dispatch } from "react";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import Switch from "@material-ui/core/Switch";
-import InputLabel from "@material-ui/core/InputLabel";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
-import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
-import IconButton from "@material-ui/core/IconButton";
+import React, { SetStateAction, Dispatch } from 'react'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+import Switch from '@material-ui/core/Switch'
+import InputLabel from '@material-ui/core/InputLabel'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
+import FormControl from '@material-ui/core/FormControl'
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward'
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward'
+import IconButton from '@material-ui/core/IconButton'
 
 const useStyles = makeStyles((theme: Theme) =>
-	createStyles({
-		controlsContainer: {
-			display: "flex",
+  createStyles({
+    controlsContainer: {
+      display: 'flex',
 
-			alignItems: "center",
-			justifyContent: "space-between",
-		},
-		control: {
-			display: "flex",
-			alignItems: "center",
-		},
-		sortButton: {},
-	})
-);
+      alignItems: 'center',
+      justifyContent: 'space-between'
+    },
+    control: {
+      display: 'flex',
+      alignItems: 'center'
+    },
+    sortButton: {}
+  })
+)
 
 const UserTaskControls = (props: {
-	filterCompleted: boolean;
-	setFilterCompleted: Dispatch<SetStateAction<boolean>>;
-	sortBy: string;
-	setSortBy: Dispatch<SetStateAction<string>>;
-	sortOrder: string;
-	setSortOrder: Dispatch<SetStateAction<string>>;
-	category: string;
-	setCategory: Dispatch<SetStateAction<string>>;
-	userCategories: string[];
+  filterCompleted: boolean
+  setFilterCompleted: Dispatch<SetStateAction<boolean>>
+  sortBy: string
+  setSortBy: Dispatch<SetStateAction<string>>
+  sortOrder: string
+  setSortOrder: Dispatch<SetStateAction<string>>
+  category: string
+  setCategory: Dispatch<SetStateAction<string>>
+  userCategories: string[]
 }) => {
-	const classes = useStyles();
+  const classes = useStyles()
 
-	const handleSortOrderChange = () => {
-		if (props.sortOrder === "desc") props.setSortOrder("asc");
-		else props.setSortOrder("desc");
-	};
+  const handleSortOrderChange = () => {
+    if (props.sortOrder === 'desc') props.setSortOrder('asc')
+    else props.setSortOrder('desc')
+  }
 
-	return (
-		<div className={classes.controlsContainer}>
-			<div className={classes.control}>
-				<InputLabel id="task-form-completed">
-					Hide completed?
-				</InputLabel>
-				<Switch
-					checked={props.filterCompleted}
-					onChange={(e) => props.setFilterCompleted(e.target.checked)}
-					name="completed"
-					inputProps={{ "aria-label": "completed checkbox" }}
-				/>
-			</div>
-			<div className={classes.control}>
-				<FormControl>
-					<InputLabel id="task-form-category">Category</InputLabel>
-					<Select
-						labelId="task-form-category"
-						id="category"
-						value={props.category}
-						onChange={(event) =>
-							props.setCategory(event.target.value as string)
-						}
-					>
-						<MenuItem value={"All"} key={0}>
-							All
-						</MenuItem>
-						{props.userCategories.map((category, index) => (
-							<MenuItem value={category} key={index + 1}>
-								{category}
-							</MenuItem>
-						))}
-					</Select>
-				</FormControl>
-			</div>
-			<div className={classes.control}>
-				<FormControl className={classes.control}>
-					<InputLabel id="task-form-sort-by">Sort</InputLabel>
-					<Select
-						labelId="task-form-sort-by"
-						id="sort-field"
-						value={props.sortBy}
-						onChange={(event) =>
-							props.setSortBy(event.target.value as string)
-						}
-					>
-						<MenuItem value={"createdAt"}>Created</MenuItem>
-						<MenuItem value={"dueDate"}>Due Date</MenuItem>
-						<MenuItem value={"description"}>Description</MenuItem>
-					</Select>
-				</FormControl>
-				{props.sortOrder === "desc" ? (
-					<IconButton
-						className={classes.sortButton}
-						onClick={handleSortOrderChange}
-					>
-						<ArrowDownwardIcon />
-					</IconButton>
-				) : (
-					<IconButton
-						className={classes.sortButton}
-						onClick={handleSortOrderChange}
-					>
-						<ArrowUpwardIcon />
-					</IconButton>
-				)}
-			</div>
-		</div>
-	);
-};
+  return (
+    <div className={classes.controlsContainer}>
+      <div className={classes.control}>
+        <InputLabel id='task-form-completed'>Hide completed?</InputLabel>
+        <Switch
+          checked={props.filterCompleted}
+          onChange={(e) => props.setFilterCompleted(e.target.checked)}
+          name='completed'
+          inputProps={{ 'aria-label': 'completed checkbox' }}
+        />
+      </div>
+      <div className={classes.control}>
+        <FormControl>
+          <InputLabel id='task-form-category'>Category</InputLabel>
+          <Select
+            labelId='task-form-category'
+            id='category'
+            value={props.category}
+            onChange={(event) =>
+              props.setCategory(event.target.value as string)
+            }
+          >
+            <MenuItem value={'All'} key={0}>
+              All
+            </MenuItem>
+            {props.userCategories.map((category, index) => (
+              <MenuItem value={category} key={index + 1}>
+                {category}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <div className={classes.control}>
+        <FormControl className={classes.control}>
+          <InputLabel id='task-form-sort-by'>Sort</InputLabel>
+          <Select
+            labelId='task-form-sort-by'
+            id='sort-field'
+            value={props.sortBy}
+            onChange={(event) => props.setSortBy(event.target.value as string)}
+          >
+            <MenuItem value={'createdAt'}>Created</MenuItem>
+            <MenuItem value={'dueDate'}>Due Date</MenuItem>
+            <MenuItem value={'description'}>Description</MenuItem>
+          </Select>
+        </FormControl>
+        {props.sortOrder === 'desc' ? (
+          <IconButton
+            className={classes.sortButton}
+            onClick={handleSortOrderChange}
+          >
+            <ArrowDownwardIcon />
+          </IconButton>
+        ) : (
+          <IconButton
+            className={classes.sortButton}
+            onClick={handleSortOrderChange}
+          >
+            <ArrowUpwardIcon />
+          </IconButton>
+        )}
+      </div>
+    </div>
+  )
+}
 
-export default UserTaskControls;
+export default UserTaskControls
--- src/components/UserTasks/UserTasks.tsx
+++ src/components/UserTasks/UserTasks.tsx
@@ -1,141 +1,138 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 import {
-	getUserTasks,
-	deleteTask,
-	updateTask,
-	getUserCategories,
-} from "../auth/utils";
-import useUser from "../auth/useUser";
-import Task, { TaskInterface } from "../Task/Task";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import AddCircle from "@material-ui/icons/AddCircle";
-import IconButton from "@material-ui/core/IconButton";
-import { Link } from "react-router-dom";
-import UserTaskControls from "../UserTaskControls/UserTaskControls";
+  getUserTasks,
+  deleteTask,
+  updateTask,
+  getUserCategories
+} from '../auth/utils'
+import useUser from '../auth/useUser'
+import Task, { TaskInterface } from '../Task/Task'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+import AddCircle from '@material-ui/icons/AddCircle'
+import IconButton from '@material-ui/core/IconButton'
+import { Link } from 'react-router-dom'
+import UserTaskControls from '../UserTaskControls/UserTaskControls'
 
 const useStyles = makeStyles((theme: Theme) =>
-	createStyles({
-		taskContainer: {
-			display: "flex",
-			alignItems: "center",
-			flexDirection: "column",
-		},
-		actionButton: {
-			padding: theme.spacing(1),
-		},
-	})
-);
+  createStyles({
+    taskContainer: {
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column'
+    },
+    actionButton: {
+      padding: theme.spacing(1)
+    }
+  })
+)
 
 const UserTasks = () => {
-	const [tasks, setTasks] = useState<TaskInterface[]>([]);
-	const [loaded, setLoaded] = useState<boolean>(false);
-	const [sortBy, setSortBy] = useState<string>("createdAt");
-	const [sortOrder, setSortOrder] = useState<string>("desc");
-	const [filterCompleted, setFilterCompleted] = useState<boolean>(true);
-	const [category, setCategory] = useState<string>("All");
-	const [categories, setCategories] = useState<string[]>([]);
-	const { user, setUser } = useUser();
-	const classes = useStyles();
-	const outstandingTasks = tasks.length
-		? tasks.filter((task) => {
-				return !task.completed;
-		  }).length
-		: 0;
-	const overdueTasks = tasks.length
-		? tasks.filter((task) => {
-				const overDue = task.dueDate
-					? Date.parse(task.dueDate!) < Date.now()
-					: false;
-				return !task.completed && overDue;
-		  }).length
-		: 0;
+  const [tasks, setTasks] = useState<TaskInterface[]>([])
+  const [loaded, setLoaded] = useState<boolean>(false)
+  const [sortBy, setSortBy] = useState<string>('createdAt')
+  const [sortOrder, setSortOrder] = useState<string>('desc')
+  const [filterCompleted, setFilterCompleted] = useState<boolean>(true)
+  const [category, setCategory] = useState<string>('All')
+  const [categories, setCategories] = useState<string[]>([])
+  const { user, setUser } = useUser()
+  const classes = useStyles()
+  const outstandingTasks = tasks.length
+    ? tasks.filter((task) => {
+        return !task.completed
+      }).length
+    : 0
+  const overdueTasks = tasks.length
+    ? tasks.filter((task) => {
+        const overDue = task.dueDate
+          ? Date.parse(task.dueDate!) < Date.now()
+          : false
+        return !task.completed && overDue
+      }).length
+    : 0
 
-	const getTasks = async () => {
-		const tasks = await getUserTasks(
-			user.token,
-			sortBy,
-			sortOrder,
-			filterCompleted,
-			category
-		);
-		if ((tasks as { error: string }).error) {
-			setUser({
-				token: "",
-			});
-			window.location.href = "/";
-		} else {
-			setTasks(tasks as TaskInterface[]);
-			setLoaded(true);
-		}
-	};
+  const getTasks = async () => {
+    const tasks = await getUserTasks(
+      user.token,
+      sortBy,
+      sortOrder,
+      filterCompleted,
+      category
+    )
+    if ((tasks as { error: string }).error) {
+      setUser({
+        token: ''
+      })
+      window.location.href = '/'
+    } else {
+      setTasks(tasks as TaskInterface[])
+      setLoaded(true)
+    }
+  }
 
-	const getCategories = async () => {
-		const userCategories = await getUserCategories(
-			user.token,
-			filterCompleted
-		);
-		setCategories(userCategories);
-	};
+  const getCategories = async () => {
+    const userCategories = await getUserCategories(user.token, filterCompleted)
+    setCategories(userCategories)
+  }
 
-	// update tasks when filters change
-	useEffect(() => {
-		getTasks();
-	}, [filterCompleted, sortBy, sortOrder, category]);
+  // update tasks when filters change
+  useEffect(() => {
+    getTasks()
+  }, [filterCompleted, sortBy, sortOrder, category])
 
-	// update categories when filterCompleted change
-	useEffect(() => {
-		getCategories();
-	}, [filterCompleted]);
+  // update categories when filterCompleted change
+  useEffect(() => {
+    getCategories()
+  }, [filterCompleted])
 
-	const handleComplete = async (id: string, completed: boolean) => {
-		await updateTask(user.token, id, { completed: !completed });
-		getTasks();
-	};
+  const handleComplete = async (id: string, completed: boolean) => {
+    await updateTask(user.token, id, { completed: !completed })
+    getTasks()
+  }
 
-	const handleDelete = async (id: string) => {
-		await deleteTask(user.token, id);
-		getTasks();
-	};
+  const handleDelete = async (id: string) => {
+    await deleteTask(user.token, id)
+    getTasks()
+  }
 
-	return loaded ? (
-		<>
-			<UserTaskControls
-				filterCompleted={filterCompleted}
-				setFilterCompleted={setFilterCompleted}
-				sortBy={sortBy}
-				setSortBy={setSortBy}
-				sortOrder={sortOrder}
-				setSortOrder={setSortOrder}
-				category={category}
-				setCategory={setCategory}
-				userCategories={categories}
-			/>
-			<div className={classes.taskContainer}>
-				<h3>
-					You have {outstandingTasks} tasks to complete.{" "}
-					{overdueTasks} tasks are overdue
-				</h3>
+  return loaded ? (
+    <>
+      <UserTaskControls
+        filterCompleted={filterCompleted}
+        setFilterCompleted={setFilterCompleted}
+        sortBy={sortBy}
+        setSortBy={setSortBy}
+        sortOrder={sortOrder}
+        setSortOrder={setSortOrder}
+        category={category}
+        setCategory={setCategory}
+        userCategories={categories}
+      />
+      <div className={classes.taskContainer}>
+        <h3>
+          You have {outstandingTasks} tasks to complete. {overdueTasks}{' '}
+          {overdueTasks === 1 ? 'task is overdue' : 'tasks are overdue'}
+        </h3>
 
-				{tasks.length
-					? tasks.map((task) => (
-							<Task
-								task={task}
-								key={task._id}
-								handleComplete={handleComplete}
-								handleDelete={handleDelete}
-							/>
-					  ))
-					: null}
-				<Link to="/tasks/new">
-					<IconButton className={classes.actionButton}>
-						<AddCircle />
-					</IconButton>
-				</Link>
-			</div>
-		</>
-	) : (
-		<h1>Loading tasks</h1>
-	);
-};
+        {tasks.length
+          ? tasks.map((task) => (
+              <Task
+                task={task}
+                key={task._id}
+                handleComplete={handleComplete}
+                handleDelete={handleDelete}
+              />
+            ))
+          : null}
+        <Link to='/tasks/new'>
+          <IconButton className={classes.actionButton}>
+            <AddCircle />
+          </IconButton>
+        </Link>
+      </div>
+    </>
+  ) : (
+    <h1>Loading tasks</h1>
+  )
+}
 
-export default UserTasks;
+export default UserTasks
