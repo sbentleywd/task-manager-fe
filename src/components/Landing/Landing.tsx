8b034ac122a9632afb6ad5204b9e2e2996a023e4
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2021-12-28 18:22:35 +0000

    Add landing page

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Button from '@material-ui/core/Button'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    landingContainer: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      alignItems: 'center',
+      flexDirection: 'column'
+    }
+  })
+)
 
 const Landing = () => {
-	return <div>Welcome to Task-Manager</div>;
-};
+  const classes = useStyles()
 
-export default Landing;
+  return (
+    <div className={classes.landingContainer}>
+      <h1>Welcome to Task-Manager. Please login/sign up</h1>
+      <div>
+        <Button
+          color='primary'
+          component={Link}
+          to={'/login'}
+          variant='contained'
+        >
+          Login
+        </Button>
+        <Button color='primary' component={Link} to={'/signup'}>
+          Sign Up
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default Landing
